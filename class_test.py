e64090844776075891b0caa99735e9330d3ac777
yangyilin182 <694673084@qq.com>
2020-02-12 13:29:16 +0800

    Initial commit

---
+++ class_test.py
@@ -0,0 +1,13 @@
+import sys
+
+
+class A:
+    pass
+
+class B:
+    pass
+
+
+print(A)
+print(B)
+print(sys.path)
---
+++ testcase/test_fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+import requests
+
+@pytest.fixture(scope="module")
+def topics():
+    return requests.get("https://testerhome.com/api/v3/topics.json?limit=2").json()
+
+def test_1(topics):
+    assert len(topics["topics"])==2
+
+def test_2(topics):
+    assert topics["topics"][0]["deleted"] == False
+
